fc7ef931971835491d3dc68399604636507ef589
nada-mah <cutenada.lara4@gmail.com>
2024-03-16 21:40:58 +0200

    add funtions to add and get reviews error handle API call

--- Project/recipe.py
+++ Project/recipe.py
@@ -1,37 +1,65 @@
 import requests 
 
 favourites = []
+ratings = []
 
 def addFavourites(id):
     if id not in favourites:
         favourites.append(id)
     
-
 def getFavourites():
     favouritescard = []
     for fav in favourites:
         favouritescard.append(moreinfoAPI(fav))
     return favouritescard
 
+def addrating(id,stars,review,title,img,email):
+    stars = int(stars)
+    id= int(id)
+    starsl = [False,False,False,False,False]
+    name=email.split('@', 1)[0]
+    for i in range(stars):
+         starsl[i]=True
+    if len(ratings) == 0:
+        reviews = {'id':id,
+                'name':name,
+                'email':email,
+                'stars' :starsl,
+                'comment': review,
+                'title':title,
+                'image':img
+                }
+        ratings.append(reviews)
+    else:
+        for rate in ratings:
+            if email == rate['email'] and id == rate['id']:
+                return ratings
+        reviews = {'id':id,
+                'name':name,
+                'email':email,
+                'stars' :starsl,
+                'comment': review,
+                'title':title,
+                'image':img
+                }
+        ratings.append(reviews)
+    return ratings
 
-def edamamAPI(data):    
-    URL = f"https://api.edamam.com/api/recipes/v2?q={data}&type=public&app_id=846c5414&app_key=c4dee5f560b23f6cc8c25800f61ad99f"
-
-    res2 = requests.get(URL)
-    res = res2.json()
-    results = res["hits"]
-    return results
+def getratings():
+    return ratings
 
 def spoonacularAPI(data):
     URL = f"https://api.spoonacular.com/recipes/complexSearch?query={data}&number=20"
 
     headers = {"Content-Type": 'application/json', "x-api-key": '362e243efc90427aa66306c8c266ee01'}
     
-
-    res2 = requests.get(URL , headers=headers)
-    res = res2.json() 
-    res = res["results"]
-    
+    try:
+        res2 = requests.get(URL , headers=headers)
+        res = res2.json() 
+        res = res["results"]
+    except Exception as e:
+        print('Error',e)
+        res={}
     return res
 
 def moreinfoAPI(id):
@@ -40,10 +68,11 @@ def moreinfoAPI(id):
 
     headers = {"Content-Type": 'application/json', "x-api-key": '7e5fee6cfa1a46deb8ee9421541cbd2d'}
     
-
-    res2 = requests.get(URL , headers=headers)
-    res = res2.json() 
+    try:
+        res2 = requests.get(URL , headers=headers)
+        res = res2.json() 
     # res = res['vegetarian']
-    
+    except Exception as e:
+        print('Error',e)
+        res={}
     return res
-
