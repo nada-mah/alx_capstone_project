bf4bcdf3d1b8929d4ae3eb01d204a675b115065a
nada-mah <cutenada.lara4@gmail.com>
2024-03-18 23:59:03 +0200

    Add database config and edit recipe route to write to db add review route

--- Project/app.py
+++ Project/app.py
@@ -5,10 +5,36 @@ import requests
 from recipe import *
 
 
+db_username = 'root'
+db_password = ";pts2f(M"
+db_name = 'recipe'
+db_host = 'localhost'
+
 app = Flask(__name__)
 
-Reviews =getratings()
+############################# SETUP ############################
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://{}:{}@{}/{}".format(db_username, db_password,db_host ,db_name )
+###############################################################
 
+db = SQLAlchemy(app)
+
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer)
+    email = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120))
+    comment = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    image = db.Column(db.String(120))
+    stars = db.Column(db.Integer)
+    
+# Create the database tables
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+create_tables()
 
 """Home"""
 @app.route('/')
@@ -30,20 +56,32 @@ def recipe_template():
     if request.method == 'POST':
         id = request.form.get('id')
         results = moreinfoAPI(id)
-        return render_template('recipe.html', Reviews=Reviews,recipe=results)
+        userreview = Reviews.query.filter_by(recipe_id=id).all()
+        if userreview:
+            riv = userreview
+        else:
+            riv = {}
+        return render_template('recipe.html', stars=starsdisp,Reviews=riv,recipe=results)
     return render_template("index.html")
 
-@app.route('/recipe/review' , methods = ["GET","POST"])
+@app.route('/review' , methods = ["POST"])
 def review_template():
     if request.method == 'POST':
-        id = request.form.get('id')
-        stars = request.form.get('stars')
+        recipe_id = int(request.form.get('id'))
+        stars = int(request.form.get('stars'))
         review = request.form.get('review')
         title = request.form.get('title')
         img = request.form.get('img')
         email = request.form.get('email')
-        Reviews = addrating(id,stars,review,title,img,email)
-        return render_template('recipe.html',recipe=moreinfoAPI(id),Reviews=Reviews)
+        name=email.split('@', 1)[0]
+        userreview = Reviews(recipe_id=recipe_id,name=name,email=email,stars=stars,title=title,comment=review,image=img)
+        try:
+            with app.app_context():
+                db.session.add(userreview)
+                db.session.commit()
+                return render_template('review.html', stars=starsdisp,Review=userreview)
+        except Exception as error:
+            return error
     return render_template("index.html")
 
 
